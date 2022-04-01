ea19eee625343fb858c24f872204ba407e6945c4
Jason-5-37 <xrf4650@aut.ac.nz>
2022-04-01 18:36:29 +1300

    make it better

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-// server.js
-
+// app.js
 
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 // routes
 const books = require('./routes/api/books');
 
-const server = express();
+const app = express();
 
 // Connect Database
 connectDB();
 
 // cors
-server.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-server.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 // use Routes
-server.use('/api/books', books);
+app.use('/api/books', books);
 
 // Accessing the path module
 const path = require("path");
 
 // Step 1:
-server.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
-server.get("/", function (request, response) {
+app.get("*", function (request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
+
+
+const PORT = process.env.PORT || 8082;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
