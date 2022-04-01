15e85185a6bea35bec4c80fd0d427b0c7ecfdd93
Jason-5-37 <xrf4650@aut.ac.nz>
2022-04-01 18:26:10 +1300

    make it better

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ const path = require("path");
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
-app.get("*", function (request, response) {
+app.get("/", function (request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
