3698e6592adf65551e2613982a92019ae7b355ad
Jason-5-37 <xrf4650@aut.ac.nz>
2022-04-01 17:24:21 +1300

    make it better

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 // use Routes
 app.use('/api/books', books);
 
