28b116c970fdd30e2f4d9a24627d28d179fdb9a9
Jason-5-37 <xrf4650@aut.ac.nz>
2022-04-01 18:19:38 +1300

    make it better

--- server.js
+++ server.js
@@ -30,8 +30,3 @@ app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.get("*", function (request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
