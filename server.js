368a19c136d41710e976ab768d152fc524958e49
Jason-5-37 <xrf4650@aut.ac.nz>
2022-04-01 17:50:26 +1300

    make it better

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.get("*", function (request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-
-const port = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
